daf2199a190a8c3fba457e13643bb1648047eb24
786-go <noorullahw@gmail.com>
2022-05-26 22:01:11 -0700

    completed - can't get the links to work

--- prflGENRTR.js
+++ prflGENRTR.js
@@ -6,15 +6,107 @@ const { generatePage } = require('./src/profilePAGE-template');
 
 const promptUser = () => {
     return inquirer.prompt([
+
         {
             type: 'input',
-            name: 'projectTitle',
-            message: 'What is the title of your project?'
+            name: 'name1',
+            message: 'Note: This program only works for teams with 4 people. What is the name of team member 1?'
         },
         {
             type: 'input',
-            name: 'github',
-            message: 'What is your Github username?'
+            name: 'id1',
+            message: 'What is his/her employee ID?'
+        },
+        {
+            type: 'input',
+            name: 'office1',
+            message: 'What is his/her office number'
+        },
+        {
+            type: 'input',
+            name: 'email1',
+            message: 'What is his/her email address?'
+        },
+        {
+            type: 'input',
+            name: 'github1',
+            message: 'What is his/her Github username?'
+        },
+        {
+            type: 'input',
+            name: 'name2',
+            message: 'What is the name of team member 2?'
+        },
+        {
+            type: 'input',
+            name: 'id2',
+            message: 'What is his/her employee ID?'
+        },
+        {
+            type: 'input',
+            name: 'office2',
+            message: 'What is his/her office number'
+        },
+        {
+            type: 'input',
+            name: 'email2',
+            message: 'What is his/her email address?'
+        },
+        
+        {
+            type: 'input',
+            name: 'github2',
+            message: 'What is his/her Github username?'
+        },
+        {
+            type: 'input',
+            name: 'name3',
+            message: 'What is the name of team member 3?'
+        },
+        {
+            type: 'input',
+            name: 'id3',
+            message: 'What is his/her employee ID?'
+        },
+        {
+            type: 'input',
+            name: 'office3',
+            message: 'What is his/her office number'
+        },
+        {
+            type: 'input',
+            name: 'email3',
+            message: 'What is his/her email address?'
+        },
+        {
+            type: 'input',
+            name: 'github3',
+            message: 'What is his/her Github username?'
+        },
+        {
+            type: 'input',
+            name: 'name4',
+            message: 'What is the name of team member 4?'
+        },
+        {
+            type: 'input',
+            name: 'id4',
+            message: 'What is his/her employee ID?'
+        },
+        {
+            type: 'input',
+            name: 'office4',
+            message: 'What is his/her office number'
+        },
+        {
+            type: 'input',
+            name: 'email4',
+            message: 'What is his/her email address?'
+        },
+        {
+            type: 'input',
+            name: 'github4',
+            message: 'What is his/her Github username?'
         }
     ])
 }
--- src/profilePAGE-template.js
+++ src/profilePAGE-template.js
@@ -8,22 +8,79 @@ const generatePage = (generatePage) => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>${generatePage.projectTitle} READme</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
       </head>
     
       <body>
-        
-        <h1>${generatePage.projectTitle}</h1>
+     
+<h1 class="p-3 mb-2 bg-info text-dark text-center">MY TEAM</h1>
 
-        ${generatePage.github}
+<div class="w-50 p-3">
+
+    <div class="card text-left">
+        <div class="card-header">
+
+            <ul class="nav nav-pills card-header-pills">
+            <li class="nav-item">
+            <a class="nav-link active" href="#mem1">${generatePage.name1}</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#mem2">${generatePage.name2}</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#mem3">${generatePage.name3}</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#mem4">${generatePage.name4}</a>
+            </li>
+            </ul>
+        </div>
+
+        <div class="card-body" id="mem1">
+            <p class="card-text">
+                Employee ID: ${generatePage.id1} <br>
+                Office number: ${generatePage.office1} <br>
+                Email address: <a href="mailto:${generatePage.email1}">${generatePage.email1}</a> <br>
+                <a href="https://github.com/${generatePage.github1}">Github account</a>
+            </p>
+        </div>
+
+        <div class="card-body" id="mem2">
+            <p class="card-text">
+                Employee ID: ${generatePage.id2} <br>
+                Office number: ${generatePage.office2} <br>
+                Email address: <a href="mailto:${generatePage.email2}">${generatePage.email2}</a> <br>
+                <a href="https://github.com/${generatePage.github2}">Github account</a>
+            </p>
+        </div>
+
+        <div class="card-body" id="mem3">
+            <p class="card-text">
+                Employee ID: ${generatePage.id3} <br>  
+                Office number: ${generatePage.office3} <br>
+                Email address: <a href="mailto:${generatePage.email3}">${generatePage.email3}</a> <br>
+                <a href="https://github.com/${generatePage.github3}">Github account</a>
+            </p>
+        </div>
+
+        <div class="card-body" id="mem4">
+            <p class="card-text">
+                Employee ID: ${generatePage.id4} <br>   
+                Office number: ${generatePage.office4} <br>
+                Email address: <a href="mailto:${generatePage.email4}">${generatePage.email4}</a> <br>
+                <a href="https://github.com/${generatePage.github4}">Github account</a>
+            </p>
+        </div>
+
+    </div>
+
+</div>
         
         </body>
         </html>
     `;
 };
-
-
-
-
 
 
 
